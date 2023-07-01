1032d0717fd7659d1fc148a00a81aa8393439088
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-01 15:16:50 -0500

    diagrama y script tablas

---
+++ tablas.sql
@@ -0,0 +1,32 @@
+create table genero(
+	id serial primary key,
+	genero varchar(50) not null
+);
+
+create table persona(
+	id serial primary key,
+	cedula varchar(10) not null,
+	nombres varchar(50) not null,
+	apellidos varchar(50) not null,
+	fecha_nacimiento date not null,
+	id_genero integer not null,
+	constraint fk_id_genero foreign key (id_genero) references genero(id)
+);
+
+create table rol_usuario(
+	id serial primary key,
+	rol varchar(50) not null,
+	descripcion varchar(50) not null,
+	constraint uk_descripcion unique(rol)
+);
+
+create table usuario(
+	id serial primary key,
+	id_persona integer not null,
+	usuario varchar(50) not null,
+	contrasena varchar(50) not null,
+	id_rol integer  not null,
+	constraint uk_usuario unique (usuario),
+	constraint fk_id_rol foreign key (id_rol) references rol_usuario(id)
+);
+

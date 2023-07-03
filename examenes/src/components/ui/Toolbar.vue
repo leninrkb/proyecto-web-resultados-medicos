de00b7c02ff0f9aede2d571b36f8cf4ef3a0499f
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-03 15:13:21 -0500

    mejoro toolbar

--- examenes/src/components/ui/Toolbar.vue
+++ examenes/src/components/ui/Toolbar.vue
@@ -1,12 +1,18 @@
 <template lang="es">
     <div class="font-sans bg-sky-500 rounded-b-lg p-3 flex-wrap sm:flex gap-3">
-        <h1 class='text-2xl font-medium p-2 animation ease-in-out duration-500 hover:text-emerald-200 hover:scale-110 hover:cursor-pointer'>
+        <h1 class='text-2xl font-medium p-2 
+        animation ease-in-out duration-300
+         hover:text-emerald-200 hover:cursor-pointer
+         '>
             {{datos.titulo}}
         </h1>
         <div class="grow">
             <nav>
                 <ul class='flex gap-3 text-sky-800'>
-                    <li class="shadow-md rounded-lg p-2 animation ease-in-out duration-150 hover:translate-y-1 hover:shadow-none" v-for="(item, index) in datos.enlaces" :key="index">
+                    <li class="shadow-md rounded-lg p-2 
+                    animation ease-in-out duration-150 
+                    hover:translate-y-1 hover:shadow-none hover:text-black" 
+                    v-for="(item, index) in datos.enlaces" :key="index">
                         {{item}}
                     </li>
                 </ul>
@@ -15,7 +21,10 @@
         <div>
             <nav>
                 <ul class='flex gap-3 text-gray-200'>
-                    <li class="shadow-md rounded-lg p-2 bg-emerald-600 animation ease-in-out duration-150 hover:translate-y-1 hover:shadow-none" v-for="(item, index) in datos.primarios" :key="index">
+                    <li class="shadow-md rounded-lg p-2 bg-emerald-600 
+                    animation ease-in-out duration-150 
+                    hover:translate-y-1 hover:shadow-none hover:text-white" 
+                    v-for="(item, index) in datos.primarios" :key="index">
                         {{item}}
                     </li>
                 </ul>

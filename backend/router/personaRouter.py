0388ba6067b9fa9a7eedb90cec655c7c4c8cea48
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-05 17:38:45 -0500

    refactorizo codigo

--- backend/router/personaRouter.py
+++ backend/router/personaRouter.py
@@ -13,27 +13,27 @@ def getDB():
     finally:
         db.close()
 
-@router_persona.post('/create_persona')
+@router_persona.post('/create')
 async def createPersona(request:RequestPersona, db:Session=Depends(getDB)):
     _persona = crud.createPersona(db, request.parameter)
     return Response(code=200, status='ok', message='se ha creado una persona', result=_persona).dict(exclude_none=True)
 
-@router_persona.get('/get_personas')
+@router_persona.get('/get')
 async def getPersonas(db:Session=Depends(getDB)):
     _personas = crud.getPersonas(db, 0, 100)
     return Response(code=200, status='ok', message='se trajo todas las personas', result=_personas).dict(exclude_none=True)
 
-@router_persona.get('/get_persona_id/{id}')
+@router_persona.get('/get/{id}')
 async def getPersonaId(id:int, db:Session=Depends(getDB)):
     _persona = crud.getPersonaId(db, id)
     return Response(code=200, status='ok', message='se trajo una persona', result=_persona).dict(exclude_none=True)
 
-@router_persona.post('/update_persona')
+@router_persona.post('/update')
 async def updatePersona(request:RequestPersona, db:Session=Depends(getDB)):
     _persona = crud.updatePersona(db, request.parameter)
     return Response(code=200, status='ok', message='se actualizo una persona', result=_persona).dict(exclude_none=True)
 
-@router_persona.delete('/delete_persona/{id}')
+@router_persona.delete('/delete/{id}')
 async def updatePersona(id:int, db:Session=Depends(getDB)):
     crud.deletePersona(db, id)
     return Response(code=200, status='ok', message='se elimino una persona').dict(exclude_none=True)

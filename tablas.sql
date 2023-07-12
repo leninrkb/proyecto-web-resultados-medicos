795b1684c93a044e877bff5ef5500e69cdbeb45d
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-12 00:56:46 -0500

    agrego tablas

--- tablas.sql
+++ tablas.sql
@@ -17,8 +17,8 @@ create table persona(
 create table rol_usuario(
 	id serial primary key,
 	rol varchar(50) not null,
-	descripcion varchar(200) not null,
-	constraint uk_descripcion unique(rol)
+	descripcion varchar(500) not null,
+	constraint uk_rol unique(rol)
 );
 
 create table usuario(
@@ -28,7 +28,61 @@ create table usuario(
 	usuario varchar(50) not null,
 	contrasena varchar(50) not null,
 	constraint uk_usuario unique (usuario),
+	constraint uk_id_persona unique (id_persona),
 	constraint fk_id_rol foreign key (id_rol) references rol_usuario(id)
     constraint fk_id_persona foreign key (id_persona) references persona(id)
+);
+
+create table estado_examen(
+	id serial primary key,
+	estado varchar(50) not null,
+	descripcion varchar(500) not null,
+	constraint uk_estado unique(estado)
+);
+
+create table tipo_examen(
+	id serial primary key,
+	tipo varchar(100) not null,
+	referencia varchar(500) not null,
+	descripcion varchar(500) not null,
+	precio numeric(7,2) not null,
+	constraint uk_tipo unique(tipo)
+);
+
+create table institucion(
+	id serial primary key,
+	institucion varchar(200) not null,
+	razon_social varchar(500) not null,
+	descripcion varchar(500) not null,
+	ubicacion varchar(500) not null,
+	path_img text not null,
+	constraint uk_institucion unique(institucion)
+);
+
+create table examen(
+	id serial primary key,
+	id_institucion integer not null,
+	id_persona integer not null,
+	id_estado integer not null,
+	examen varchar(200) not null,
+	motivo varchar(500) not null,
+	fecha_realiza date not null,
+	observacion varchar(500) not null,
+	constraint fk_id_institucion foreign key (id_institucion) references institucion(id),
+	constraint fk_id_persona foreign key (id_persona) references persona(id), 
+	constraint fk_id_estado foreign key (id_estado) references estado_examen(id) 
+);
+
+create table detalle_examen(
+	id serial primary key,
+	id_examen integer not null,
+	id_tipo integer not null,
+	id_estado integer not null,
+	resultado varchar(500) not null,
+	observacion varchar(500) not null,
+	constraint fk_id_examen foreign key (id_examen) references examen(id),
+	constraint fk_id_tipo foreign key (id_tipo) references tipo_examen(id), 
+	constraint fk_id_estado foreign key (id_estado) references estado_examen(id),
+	constraint uk_id_examen_tipo unique(id_examen, id_tipo)
 );
 

ff5347e632264a4892eed4d26ed069a0afc37beb
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-15 17:58:21 -0500

    generando qr

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = 'postgresql+psycopg2://user_postgre:FeBjjTD0iNohhn66Zp2VEQYHN6wrNGsU@dpg-cihmmh5gkuvojja3p9lg-a.oregon-postgres.render.com/examenes'
 engine = create_engine(DATABASE_URL)
-# session_local = sessionmaker(autoflush=False, bind=engine)
 SessionLocal  = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = SessionLocal()
 Base = declarative_base()
--- examenes/src/components/DetalleExamen.vue
+++ examenes/src/components/DetalleExamen.vue
@@ -20,29 +20,49 @@
             </div>
             <br>
             <div>
-                <h4 class="mx-auto w-fit">Informe de resultados</h4>
+                <h4 class="mx-auto w-fit font-semibold">Informe de resultados</h4>
             </div>
             <br>
-            <div class="text-sm">
-                <table class="table-fixed">
+            <div class="text-sm border-l-4 border-green-500 pl-2">
+                <h5 class="uppercase font-semibold text-gray-300">{{examen.examen}}</h5>
+                <h4><strong>Razon del examen</strong></h4>
+                <p class="text-justify text-gray-400">
+                    {{examen.motivo}}
+                </p>
+            </div>
+            <br>
+            <div class="overflow-x-auto text-sm rounded-lg bg-stone-700 p-2">
+                <table class="table-auto divide-y-2 divide-stone-400">
                     <thead>
                         <tr>
-                        <th>Examen</th>
-                        <th>Tipo</th>
-                        <th>Resultado</th>
-                        <th>V. Referencia</th>
-                        <th>Estado</th>
-                        <th>Observacion</th>
+                            <th class="px-2">Tipo de Examen</th>
+                            <th class="px-2">Estado</th>
+                            <th class="px-2">V. Referencia</th>
+                            <th class="px-2">Resultados</th>
+                            <th class="px-2">Observaciones</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr v-for="(item, index) in detalle" :key="index">
-                        <td>{{item.resultado}}</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
+                            <td  class="p-2">{{item.tipo}}</td>
+                            <td  class="p-2">{{item.estado}}</td>
+                            <td  class="p-2">{{item.referencia}}</td>
+                            <td  class="p-2">{{item.resultado}}</td>
+                            <td  class="p-2">{{item.observacion}}</td>
                         </tr>
                     </tbody>
                 </table> 
+            </div>
+            <br>
+            <div class="text-sm border-l-4 border-green-500 pl-2">
+                <h4><strong>Observaciones</strong></h4>
+                <p class="text-justify text-gray-400">
+                    {{examen.observacion}}
+                </p>
+            </div>
+            <button @click="crearQR">generar qr</button>
+            <div id="container">
+                <canvas id="canvas"></canvas>
             </div>
         </div>  
         <hr>
@@ -54,12 +74,14 @@
 </template>
 <script>
 import { arrows_rotate } from '../variables/svg';
-import { 
-    InstitucionService, ExamenService, 
+import {
+    InstitucionService, ExamenService,
     PersonaService, GeneroService,
-    TipoExamenService, DetalleExamenService
+    TipoExamenService, DetalleExamenService,
+    EstadoService
 } from '../variables/servicios';
 import { calcularEdad } from '../variables/funciones';
+import qrcode from 'qrcode';
 
 export default {
     name: 'DetalleExamen',
@@ -82,26 +104,58 @@ export default {
         }
     },
     methods: {
-        async cargar_datos(){
+        reestablecer() {
+            this.institucion = {},
+                this.examen = {},
+                this.persona = {},
+                this.genero = {},
+                this.tipo_examen = {},
+                this.detalle = []
+        },
+        crearQR() {
+            let canvas = document.getElementById('container');
+            qrcode.toCanvas('hola soy joao', { errorCorrectionLevel: 'H' }, function (err, canvas) {
+                if (err) throw err
+                var container = document.getElementById('container');
+                container.appendChild(canvas)
+            });
+        },
+        async cargar_datos() {
             this.mostrar = false;
             try {
+                // this.crearQR();
+                this.reestablecer();
                 this.institucion = await InstitucionService.get_instituciones_id(this.fila.id_institucion);
                 this.examen = await ExamenService.get_examen_id(this.fila.id);
                 this.persona = await PersonaService.get_persona_id(this.fila.id_persona);
                 this.persona.edad = calcularEdad(this.persona.fecha_nacimiento);
                 this.genero = await GeneroService.get_genero_id(this.persona.id_genero);
-                this.detalle = await DetalleExamenService.get_detalle(this.examen.id);
+                let tipos_examenes = await TipoExamenService.get_tipos();
+                let estados_examenes = await EstadoService.get_estados();
+                let resp = await DetalleExamenService.get_detalle();
+                resp.forEach(element => {
+                    if (element.id_examen == this.examen.id) {
+                        let tipo = tipos_examenes.find(p => p.id == element.id_tipo);
+                        element.tipo = tipo.tipo;
+                        element.referencia = tipo.referencia;
+                        let estado = estados_examenes.find(p => p.id == element.id_estado);
+                        element.estado = estado.estado;
+                        this.detalle.push(element);
+                    }
+                });
 
-            } catch (error) { }
+            } catch (error) {
+                console.log(error);
+             }
             this.mostrar = true;
 
         }
     },
-    beforeMount(){
+    beforeMount() {
         this.cargar_datos();
     },
-    watch:{
-        fila(){
+    watch: {
+        fila() {
             console.log(this.fila);
             this.cargar_datos();
         }

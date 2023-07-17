d025fc8d3f1c1db1650458986b6954288345744d
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-16 21:32:41 -0500

    resumen view

--- examenes/src/views/ResumenView.vue
+++ examenes/src/views/ResumenView.vue
@@ -1,21 +1,48 @@
 <template lang="es">
-    <div>
-        resumen view {{id_examen}}
+    <div class='container mx-auto'>
+        <Toolbar :datos=datos></Toolbar>
+        <br>
+        <div class="mx-5 sm:mx-24">
+            <div class="p-3 text-gray-400">
+                resumen
+                - portada de bienvenida
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import Toolbar from '@/components/ui/Toolbar.vue';
+import {registro, login} from '../variables/rutas';
+
 export default {
     name:'ResumenView',
+    components:{
+        Toolbar
+    },
     data() {
         return {
-            id_examen: undefined
+            datos:{
+                titulo:'Biolab',
+                enlaces:[ ],
+                primarios:[
+                    {
+                        titulo:'Registrarse',
+                        ruta: registro
+                    },
+                    {
+                        titulo:'Iniciar Sesion',
+                        ruta: login
+                    }
+                ]
+            },
+            hash: undefined
         }
     },
     methods: {
         
     },
     mounted() {
-        this.id_examen = this.$route.params.id;
+        this.hash = this.$route.params.id;
         
     },
 }

b231d00a9bf0d6b29dd4a58cceb375efc70b547c
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-17 19:25:55 -0500

    funcional 1

--- examenes/src/views/ExamenesView.vue
+++ examenes/src/views/ExamenesView.vue
@@ -3,9 +3,12 @@
         <Toolbar :datos=datos></Toolbar>
         <br>
         <div class="mx-5 sm:mx-24">
-            <div class="p-3 text-gray-400">
-                Examenes disponibles
-                - tabla con los examenes disponibles
+            <div class=" text-gray-200">
+                <h2 class="text-xl">Examenes disponibles</h2>
+            </div>
+            <div class="text-gray-200">
+                <label for=""><strong>Tipos de examenes</strong></label>
+                <Tabla :mostrar="terminado_tipos" :datos="datos_tipos" @recargar="cargar_tipos"></Tabla>
             </div>
         </div>
     </div>
@@ -13,10 +16,14 @@
 <script>
 import Toolbar from '@/components/ui/Toolbar.vue';
 import { registro, login } from '../variables/rutas';
+import { TipoExamenService } from '../variables/servicios';
+import Tabla from '../components/ui/Tabla.vue';
+
+
 export default {
     name: 'ExamenesView',
     components: {
-        Toolbar
+        Toolbar, Tabla
     },
     data() {
         return {
@@ -33,8 +40,21 @@ export default {
                         ruta: login
                     },
                 ]
-            }
+            },
+            datos_tipos: {},
+            terminado_tipos : false,
         }
+    },
+    methods: {
+        async cargar_tipos() {
+            this.terminado_tipos = false;
+            this.datos_tipos.titulos = ['tipo', 'referencia', 'descripcion', 'precio'];
+            this.datos_tipos.registros = await TipoExamenService.get_tipos();
+            this.terminado_tipos = true;
+        },
+    },
+    mounted() {
+        this.cargar_tipos();
     },
 }
 </script>
--- examenes/src/views/HomeView.vue
+++ examenes/src/views/HomeView.vue
@@ -3,9 +3,17 @@
         <Toolbar :datos=datos></Toolbar>
         <br>
         <div class="mx-5 sm:mx-24">
-            <div class="p-3 text-gray-400">
-                HOME
-                - portada de bienvenida
+            <div class="p-3 text-gray-200 text-xl font-sans ">
+                <h2 class="text-4xl font-sans">Somos <strong>{{institucion.institucion}}</strong></h2>
+                <img class="rounded-lg m-2" :src="institucion.path_img" alt="logo">
+                <h3>{{institucion.razon_social}}</h3>
+                <br>
+                <div class="border-l-4 border-teal-500 pl-2">
+                    <h3><strong>Quienes somos ?</strong></h3>
+                    <p class="text-justify text-lg">{{institucion.descripcion}}</p>
+                    <br>
+                    <h3 class=" ">Encuentranos en: {{institucion.ubicacion}}</h3>
+                </div>
             </div>
         </div>
     </div>
@@ -13,6 +21,8 @@
 <script>
 import Toolbar from '@/components/ui/Toolbar.vue';
 import {registro, examenes_disponibles, login} from '../variables/rutas';
+import { InstitucionService } from '../variables/servicios';
+
 export default {
     name: 'HomeView',
     components:{
@@ -38,8 +48,20 @@ export default {
                         ruta: login
                     }
                 ]
-            }
+            },
+            institucion: {institucion:''}
         }
-    }
+    },
+    methods: {
+        async cargar_institucion(){
+            this.institucion = await InstitucionService.get_instituciones();
+            this.institucion = this.institucion[0];
+            this.datos.titulo = this.institucion.institucion;
+            console.log(this.institucion);
+        }
+    },
+    mounted() {
+        this.cargar_institucion();
+    },
 }
 </script>

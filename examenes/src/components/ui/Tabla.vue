4b468980180c015e18a0eadc3905808807119891
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-18 14:18:40 -0500

    update tabla con buscar

--- examenes/src/components/ui/Tabla.vue
+++ examenes/src/components/ui/Tabla.vue
@@ -1,10 +1,17 @@
 <template lang="es">
     <div v-if="mostrar">
-      <div class="flex flex-row-reverse">
+      <br>
+      <div class="flex flex-wrap flex-row-reverse gap-2">
         <button class="mb-2 px-2 py-1 border-b-2 border-cyan-500 text-gray-300
         animation duration-300 hover:bg-cyan-500 hover:text-gray-100 hover:rounded-md 
         active:bg-cyan-700"
         @click="emitir_evento">Refrescar tabla</button>
+        <div class="flex-grow"></div>
+        <div class="flex flex-wrap gap-2 m-1">
+          <button @click="filtrar()" class="p-1 rounded-lg bg-teal-500 active:bg-teal-800">Buscar</button>
+          <input class="p-1 rounded-md px-2" type="text" v-model="filtro">
+          <button @click="quitar_filtro" class="p-1 underline underline-offset-2 text-gray-200 active:text-gray-500">Limpiar</button>
+        </div>
       </div>
       <div class="overflow-x-auto rounded-lg border border-gray-700">
         <table class="min-w-full divide-y-2 divide-gray-700 bg-gray-200 text-sm">
@@ -50,7 +57,8 @@ export default {
     return {
       registros: undefined,
       titulos: undefined,
-      arrows_rotate: arrows_rotate
+      arrows_rotate: arrows_rotate,
+      filtro: ''
     }
   },
   methods: {
@@ -63,6 +71,20 @@ export default {
         this.titulos = [];
       }
 
+    },
+    filtrar(){
+      let filtrados = [];
+      this.registros.forEach(p => {
+        Object.keys(p).forEach(k => {
+          if(p[k] == this.filtro){
+            filtrados.push(p);
+          }
+        });
+      });
+      this.registros = filtrados;
+    },
+    quitar_filtro(){
+      this.cargar_tabla();
     },
     emitir_evento() {
       this.$emit('recargar', true);
